f9924bc12538b85ad1583e5f3c8d549d9456136f
jwhite878 <jwhite878@gmail.com>
2016-12-24 17:46:05 -0500

    intellij git test

--- src/com/jwhite/CPU.java
+++ src/com/jwhite/CPU.java
@@ -1,8 +1,8 @@
 package com.jwhite;
 
-public class CPU {
+class CPU {
 
-    public static void main() {
-	System.out.println("Hello world (CPU)!");
+    static void main() {
+	System.out.println("Hello world (CPU)123!");
     }
 }
